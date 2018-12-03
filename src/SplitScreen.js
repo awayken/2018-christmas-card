2b81a3a41daebf56a68ed641eff99e8c0060aa41
Miles Rausch <miles@milesrausch.com>
2018-12-02 22:18:06 -0600

    Add Prettier as Git hook

--- src/SplitScreen.js
+++ src/SplitScreen.js
@@ -2,21 +2,25 @@ import React from 'react';
 import './SplitScreen.css';
 
 function SplitScreen({ children }) {
-    if (!children || children.length === 0) {
-        return null;
-    }
+  if (!children || children.length === 0) {
+    return null;
+  }
 
-    const firstChild = Array.isArray(children) ? children[0] : children
+  const firstChild = Array.isArray(children) ? children[0] : children;
 
-    let otherChildrenScreen = null
-    if (children.length > 1) {
-        otherChildrenScreen = <div className="split__screen">{children.slice(1)}</div>;
-    }
+  let otherChildrenScreen = null;
+  if (children.length > 1) {
+    otherChildrenScreen = (
+      <div className="split__screen">{children.slice(1)}</div>
+    );
+  }
 
-    return <div className="split">
-        <div className="split__screen">{firstChild}</div>
-        {otherChildrenScreen}
-      </div>;
+  return (
+    <div className="split">
+      <div className="split__screen">{firstChild}</div>
+      {otherChildrenScreen}
+    </div>
+  );
 }
 
 export default SplitScreen;
--- src/stories/index.js
+++ src/stories/index.js
@@ -6,51 +6,51 @@ import '../index.css';
 import App from '../App';
 import Quiz from '../Quiz';
 import SplitScreen from '../SplitScreen';
-import { answers } from "./data";
+import { answers } from './data';
 
 storiesOf('App', module).add('renders', () => <App />);
 
-storiesOf("Quiz", module)
-  .add("renders question", () => (
+storiesOf('Quiz', module)
+  .add('renders question', () => (
     <Quiz
       question="Which answer is the answer to this question?"
       answers={answers}
       correctAnswer="q3"
-      onAnswer={action("quiz-answer")}
+      onAnswer={action('quiz-answer')}
     />
   ))
-  .add("renders with correct answer", () => (
+  .add('renders with correct answer', () => (
     <Quiz
       question="Which answer is the answer to this question?"
       answers={answers}
       correctAnswer="q3"
       selectedAnswer="q3"
-      onAnswer={action("quiz-answer")}
+      onAnswer={action('quiz-answer')}
     />
   ))
-  .add("renders with incorrect answer", () => (
+  .add('renders with incorrect answer', () => (
     <Quiz
       question="Which answer is the answer to this question?"
       answers={answers}
       correctAnswer="q3"
       selectedAnswer="q1"
-      onAnswer={action("quiz-answer")}
+      onAnswer={action('quiz-answer')}
     />
   ));
 
-storiesOf("SplitScreen", module)
-  .add("renders 2 children", () => (
+storiesOf('SplitScreen', module)
+  .add('renders 2 children', () => (
     <SplitScreen>
       <p>Panel 1</p>
       <p>Panel 2</p>
     </SplitScreen>
   ))
-  .add("renders 1 child", () => (
+  .add('renders 1 child', () => (
     <SplitScreen>
       <p>Panel 1</p>
     </SplitScreen>
   ))
-  .add("renders more children", () => (
+  .add('renders more children', () => (
     <SplitScreen>
       <p>Panel 1</p>
       <p>Panel 2</p>
