0f46f6db5562ef7441d42252783aa24c4494722d
Miles Rausch <miles@milesrausch.com>
2018-12-16 00:05:32 -0600

    Add keyboard-based progress

--- src/App.js
+++ src/App.js
@@ -20,6 +20,27 @@ class App extends Component {
         };
     }
 
+    componentDidMount() {
+        document.body.addEventListener('keyup', this.handleArrows);
+    }
+
+    componentWillUnmount() {
+        document.body.removeEventListener('keyup', this.handleArrows);
+    }
+
+    handleArrows = event => {
+        const { startQuiz } = this.state;
+        const key = event.key;
+
+        if (startQuiz) {
+            if (key === 'ArrowRight') {
+                this.handleProgress('next');
+            } else if (key === 'ArrowLeft') {
+                this.handleProgress('back');
+            }
+        }
+    };
+
     handleAnswer = answer => {
         const { answers, currentQuestion } = this.state;
         const newAnswers = [...answers];
@@ -32,19 +53,31 @@ class App extends Component {
     };
 
     handleProgress = progress => {
+        const { questions } = this.props;
         const { currentQuestion } = this.state;
-        let newQuestion = progress.target.value;
 
-        if (newQuestion === 'next') {
-            newQuestion = currentQuestion + 1;
-        } else if (newQuestion === 'back') {
-            newQuestion = currentQuestion - 1;
+        let progressValue = progress;
+        if (progress.target) {
+            progressValue = progress.target.value;
+        }
+
+        let nextQuestion;
+        if (progressValue === 'next') {
+            nextQuestion = currentQuestion + 1;
+        } else if (progressValue === 'back') {
+            nextQuestion = currentQuestion - 1;
         } else {
-            newQuestion = parseInt(progress.target.value, 10);
+            nextQuestion = parseInt(progressValue, 10);
+        }
+
+        if (nextQuestion >= questions.length) {
+            nextQuestion = questions.length - 1;
+        } else if (nextQuestion < 0) {
+            nextQuestion = 0;
         }
 
         this.setState({
-            currentQuestion: newQuestion
+            currentQuestion: nextQuestion
         });
     };
 
